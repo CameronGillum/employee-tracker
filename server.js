f98e3a8544559b4e57ac19a072ebf68ecd907859
CameronGillum <GillumTCameron@gmail.com>
2024-07-17 17:00:55 -0400

    Adds prompts for inquirer

--- server.js
+++ server.js
@@ -1,87 +1,97 @@
+// GIVEN a command-line application that accepts user input
 const inquirer = require('inquirer');
-const Pool = require('pg').Pool;
+const { Client } = require('pg');
 
-const connection = mysql.createConnection({
+const client = new Client({
   host: 'localhost',
   user: `${process.env.DB_USER}`,
-  password: `${process.env.DB_PASSWORD}`,
-  database: `${process.env.DB_NAME}`
+  password: `${process.env.DB_PASS}`,
+  database: `${process.env.DB_NAME}`,
+  port: process.env.DB_PORT || 3001 // Default port
 });
 
-connection.connect(err => {
-  if (err) throw err;
-  mainMenu();
-});
-
-function mainMenu() {
-  inquirer.prompt({
-    name: 'action',
-    type: 'list',
-    message: 'What would you like to do?',
-    choices: [
-      'View all departments',
-      'View all roles',
-      'View all employees',
-      'Add a department',
-      'Add a role',
-      'Add an employee',
-      'Update an employee role',
-      'Exit'
-    ]
-  }).then(answer => {
-    switch (answer.action) {
-      case 'View all departments':
-        viewAllDepartments();
-        break;
-      case 'View all roles':
-        viewAllRoles();
-        break;
-      case 'View all employees':
-        viewAllEmployees();
-        break;
-      case 'Add a department':
-        addADepartment();
-        break;
-      case 'Add a role':
-        addARole();
-        break;
-      case 'Add an employee':
-        addAnEmployee();
-        break;
-      case 'Update an employee role':
-        updateAnEmployeeRole();
-        break;
-      case 'Exit':
-        connection.end();
-        break;
+// WHEN I start the application
+// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+async function mainMenu() {
+    try {
+      await client.connect();
+      const answers = await inquirer.prompt([
+          {
+              type: 'list',
+              name: 'action',
+              message: 'What would you like to do?',
+              choices: [
+              'View all departments',
+              'View all roles',
+              'View all employees',
+              'Add a department',
+              'Add a role',
+              'Add an employee',
+              'Update an employee role',
+              'Quit'
+              ]
+          }
+          ]);
+  
+          switch (answers.action) {
+              case 'View all departments':
+                  await viewDepartments();
+                  break;
+              case 'View all roles':
+                  await viewRoles();
+                  break;
+              case 'View all employees':
+                  await viewEmployees();
+                  break;
+              case 'Add a department':
+                  await addDepartment();
+                  break;
+              case 'Add a role':
+                  await addRole();
+                  break;
+              case 'Add an employee':
+                  await addEmployee();
+                  break;
+              case 'Update an employee role':
+                  await updateEmployeeRole();
+                  break;
+              case 'Quit':
+                  console.log('Goodbye!');
+                  process.exit(0);
+                  break;
+          }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    } finally {
+      await Client.end();
     }
-  });
-}
+  }
+// WHEN I choose to view all departments
 
-function viewAllDepartments() {
-  // Implementation for viewing all departments
-}
+// THEN I am presented with a formatted table showing department names and department ids
 
-function viewAllRoles() {
-  // Implementation for viewing all roles
-}
+// WHEN I choose to view all roles
 
-function viewAllEmployees() {
-  // Implementation for viewing all employees
-}
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 
-function addADepartment() {
-  // Implementation for adding a department
-}
+// WHEN I choose to view all employees
 
-function addARole() {
-  // Implementation for adding a role
-}
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 
-function addAnEmployee() {
-  // Implementation for adding an employee
-}
+// WHEN I choose to add a department
 
-function updateAnEmployeeRole() {
-  // Implementation for updating an employee role
-}
+// THEN I am prompted to enter the name of the department and that department is added to the database
+
+// WHEN I choose to add a role
+
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+
+// WHEN I choose to add an employee
+
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+
+// WHEN I choose to update an employee role
+
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+
+mainMenu(); // Start the application
